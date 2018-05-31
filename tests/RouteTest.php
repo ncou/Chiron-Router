47c4ca69b6fd057f5d42148177e5f315d380d7d2
ncou <nicolas_courade@yahoo.fr>
2018-05-31 23:26:30 +0200

    first update

---
+++ tests/RouteTest.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chiron\Tests\Routing;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @covers \Zend\Expressive\Router\RouteResult
+ */
+class RouteTest extends TestCase
+{
+    public function testConstructor()
+    {
+    }
+}
